c4de285d6376190c18591efbead27f28c81a6295
Winston Tang <winston@samuraipunk.com>
2016-01-31 11:56:43 +1100

    Endgame screen

--- ggj2016_unity/Assets/Scripts/GUI/PatchNotes.cs
+++ ggj2016_unity/Assets/Scripts/GUI/PatchNotes.cs
@@ -11,9 +11,20 @@ public class PatchNotes : MonoBehaviour
 
     public TweenScale scale;
 
+    // grade
+    private bool _showingGrade;
+    public UILabel gradeLabel;
+    public TweenAlpha gradeAlpha;
+    public TweenScale gradeScale;
+
+    public AudioSource audio;
+    public AudioClip textBlip;
+    public AudioClip gradeBlip;
+
     protected void Awake()
     {
         Instance = this;
+        gameObject.SetActive(false);
     }
 
     public void SetText(string title, string notes)
@@ -25,12 +36,58 @@ public class PatchNotes : MonoBehaviour
 
         scale.ResetToBeginning();
         scale.PlayForward();
+    }
+
+    public void ShowFinalGrade()
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(ShowFinalGradeCoroutine());
+    }
+
+    private IEnumerator ShowFinalGradeCoroutine()
+    {
+        _showingGrade = true;
+        gradeAlpha.ResetToBeginning();
+        gradeScale.ResetToBeginning();
+        SetText("RAID OVER", "");
+
+        yield return new WaitForSeconds(1f);
+
+        notesLabel.text = string.Format("Final fun p/s: {0} \n", FunSystem.FunPerSecond);
+        audio.PlayOneShot(textBlip);
+
+        yield return new WaitForSeconds(1f);
+
+        notesLabel.text += string.Format("Total fun: {0}", FunSystem.TotalFun);
+        audio.PlayOneShot(textBlip);
+
+
+        yield return new WaitForSeconds(1f);
+
+        notesLabel.text += string.Format("\n\nFinal grade:");
+        audio.PlayOneShot(textBlip);
+
+
+        yield return new WaitForSeconds(1f);
+
+        // todo: grading system
+        gradeLabel.text = FunSystem.GetFinalGrade();
+
+        gradeAlpha.PlayForward();
+        gradeScale.PlayForward();
+
+        yield return new WaitForSeconds(0.2f);
+        audio.PlayOneShot(gradeBlip);
+
+        yield return new WaitForSeconds(1f);
+
+        _showingGrade = false;
 
     }
 
     public void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && !_showingGrade)
         {
             CloseNotes();
         }
@@ -39,6 +96,5 @@ public class PatchNotes : MonoBehaviour
     public void CloseNotes()
     {
         gameObject.SetActive(false);
-        
     }
 }
